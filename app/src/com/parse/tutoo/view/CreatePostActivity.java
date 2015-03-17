7cedd956fb45d30671af42998057687337299e37
hilaryhuo <hilary.huo@gmail.com>
2015-03-16 21:57:22 -0400

    added markets two spinners to new post activity

--- app/src/com/parse/tutoo/view/CreatePostActivity.java
+++ app/src/com/parse/tutoo/view/CreatePostActivity.java
@@ -14,6 +14,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -23,9 +25,14 @@ import com.parse.ParseUser;
 import com.parse.SaveCallback;
 import com.parse.tutoo.R;
 import com.parse.tutoo.model.Category;
+import com.parse.tutoo.model.Market;
+import com.parse.tutoo.model.MarketPost;
 import com.parse.tutoo.model.Post;
 import com.parse.tutoo.model.State;
 import com.parse.tutoo.util.Dispatcher;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class CreatePostActivity extends ActionBarActivity {
@@ -46,8 +53,68 @@ public class CreatePostActivity extends ActionBarActivity {
         context = getApplicationContext();
         manager = (LocationManager)context.getSystemService(Context.LOCATION_SERVICE );
         setContentView(R.layout.activity_new_post);
+        setupUI();
     }
 
+    private void setSubSpinner(Class<?> cls, Spinner spinner) {
+        List<String> list = new ArrayList<>();
+        if (cls == Category.class) {
+            for (Category element : Category.values()) {
+                if (element == Category.All) {
+                    continue;
+                }
+                list.add(element.toString());
+            }
+        }
+        else if (cls == Market.class) {
+            for (Market element: Market.values()) {
+                if (element == Market.All) {
+                    continue;
+                }
+                list.add(element.toString());
+            }
+        }
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
+    }
+    private void setupUI() {
+        final Spinner feedbackSpinner = (Spinner) findViewById(R.id.SpinnerFeedbackType);
+        final Spinner feedbackSubSpinner = (Spinner) findViewById(R.id.SpinnerFeedbackSubType);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.newPostSpinnerList, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        feedbackSpinner.setAdapter(adapter);
+        System.out.println("onCreate setupUI");
+        feedbackSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                System.out.println("position=" + parent.getItemAtPosition(position).toString());
+                if (parent.getItemAtPosition(position).toString().equals(getString(R.string.spinnerItem1Services))) {
+                    // refresh list, service list
+                    // setcategory list
+                    EditText editText = (EditText)findViewById(R.id.skillsets);
+                    editText.setHint("  Skillsets");
+                    System.out.println("++++");
+                    setSubSpinner(Category.class, feedbackSubSpinner);
+
+                } else if (parent.getItemAtPosition(position).toString().equals(getString(R.string.spinnerItem2Markets))) {
+                    // refresh list
+                    EditText editText = (EditText)findViewById(R.id.skillsets);
+                    editText.setHint("  Tags");
+                    System.out.println("====");
+                    setSubSpinner(Market.class, feedbackSubSpinner);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // do nothing
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -86,6 +153,7 @@ public class CreatePostActivity extends ActionBarActivity {
         final EditText emailField = (EditText) findViewById(R.id.inputTitle);
         description = emailField.getText().toString();
 
+        // skillsets or tags
         final EditText skillSets = (EditText) findViewById(R.id.skillsets);
         skillsets = skillSets.getText().toString();
 
@@ -94,29 +162,57 @@ public class CreatePostActivity extends ActionBarActivity {
         }
 
         final Spinner feedbackSpinner = (Spinner) findViewById(R.id.SpinnerFeedbackType);
-        String category = feedbackSpinner.getSelectedItem().toString();
-        Category enumCategory = Category.valueOf(category);
-        //ParseObject testobject = new ParseObject("Test"); testobject.put("customerName", "John"); testobject.saveInBackground();
+        final Spinner feedbackSubSpinner = (Spinner) findViewById(R.id.SpinnerFeedbackSubType);
+        if (feedbackSpinner.getSelectedItem().toString().equals(getString(R.string.spinnerItem1Services))) {
+            String category = feedbackSubSpinner.getSelectedItem().toString();
+            Category enumCategory = Category.valueOf(category);
+            //ParseObject testobject = new ParseObject("Test"); testobject.put("customerName", "John"); testobject.saveInBackground();
 
-        final Post userPost = new Post();
-        location = new Location("provider");
-        location.setLatitude(43.472285);
-        location.setLongitude(-80.544858);
+            final Post userPost = new Post();
+            location = new Location("provider");
+            location.setLatitude(43.472285);
+            location.setLongitude(-80.544858);
 
-        userPost.setUser(ParseUser.getCurrentUser().getObjectId());
-        userPost.setTitle(title);
-        userPost.setDescription(description);
-        userPost.setCategory(enumCategory);
-        userPost.setSkills(skillsets);
-        userPost.setGeoPoints(location);
+            userPost.setUser(ParseUser.getCurrentUser().getObjectId());
+            userPost.setTitle(title);
+            userPost.setDescription(description);
+            userPost.setCategory(enumCategory);
+            userPost.setSkills(skillsets);
+            userPost.setGeoPoints(location);
 
-        userPost.saveInBackground(new SaveCallback() {
-            public void done(ParseException e) {
-                if (e != null) {
-                    System.out.println(e.getMessage());
+            userPost.saveInBackground(new SaveCallback() {
+                public void done(ParseException e) {
+                    if (e != null) {
+                        System.out.println(e.getMessage());
+                    }
                 }
-            }
-        });
+            });
+        } else if (feedbackSpinner.getSelectedItem().toString().equals(getString(R.string.spinnerItem2Markets))) {
+            String market = feedbackSubSpinner.getSelectedItem().toString();
+            Market enumMarket = Market.valueOf(market);
+            //ParseObject testobject = new ParseObject("Test"); testobject.put("customerName", "John"); testobject.saveInBackground();
+
+            final MarketPost userPost = new MarketPost();
+            location = new Location("provider");
+            location.setLatitude(43.472285);
+            location.setLongitude(-80.544858);
+
+            userPost.setUser(ParseUser.getCurrentUser().getObjectId());
+            userPost.setTitle(title);
+            userPost.setDescription(description);
+            userPost.setMarket(enumMarket);
+            //userPost.setTags(tags);
+            userPost.setGeoPoints(location);
+
+            userPost.saveInBackground(new SaveCallback() {
+                public void done(ParseException e) {
+                    if (e != null) {
+                        System.out.println(e.getMessage());
+                    }
+                }
+            });
+        }
+
        /* new AlertDialog.Builder(this)
                 .setTitle(title)
                 .setMessage(description)
